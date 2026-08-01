cbd68eeaae7edd1e4f60ec3ef53125df8f643fce
gang49358-create <gang49358@gmail.com>
2026-08-02 03:06:10 +1000

    Create app.js

---
+++ app.js
@@ -0,0 +1,16 @@
+const register = document.querySelector(".register");
+const login = document.querySelector(".login");
+
+
+register.onclick = function(){
+
+    alert("Регистрация скоро будет добавлена 🚀");
+
+}
+
+
+login.onclick = function(){
+
+    alert("Вход скоро будет добавлен 🔐");
+
+}
